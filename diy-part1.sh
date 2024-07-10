e050fe2988ff8d6ea667069eadce354b8261b3ef
Starlight <flyingstarlightx@gmail.com>
2024-07-10 22:24:39 +0800

    Sync to Railgun

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,23 +1,17 @@
 #!/bin/bash
-#
-# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
-#
-# This is free software, licensed under the MIT License.
-# See /LICENSE for more information.
-#
-# https://github.com/P3TERX/Actions-OpenWrt
-# File name: diy-part1.sh
-# Description: OpenWrt DIY script part 1 (Before Update feeds)
-#
 
-# Uncomment a feed source
-#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+function git_sparse_clone() {
+    git clone --filter=blob:none --no-checkout --depth=1 $1 $2 && cd $2
+    git sparse-checkout init --cone
+    git sparse-checkout set $3
+    git checkout
+    mv $3 ../$4
+    cd ../ && rm -rf $2
+}
 
-# Add a feed source
-#echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+# Enable helloworld
+sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
-# echo '添加omcproxy iptvhelper软件源'
-#git clone https://github.com/smsmail2022/luci-app-omcproxy luci-app-omcproxy
-#git clone https://github.com/riverscn/luci-app-omcproxy.git package/luci-app-omcproxy
-#git clone -b https://github.com/riverscn/openwrt-iptvhelper.git package/luci-app-iptvhelper
+# Add passwall
+echo 'src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main' >> feeds.conf.default
+echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main' >> feeds.conf.default    
